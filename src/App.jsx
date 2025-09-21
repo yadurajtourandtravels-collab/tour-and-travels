e9be6a89bfff0d6a0411ec96a4eb2ee2d20bba74
NikT404 <nikhilsinghrajawat0@gmail.com>
2025-09-21 20:38:38 +0530

    updation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Hero from './components/hero';
+import Footer from './components/footer';
 import './index.css'; // Import your main stylesheet
 
 const App = () => {
